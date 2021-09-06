65b12fc6090c544a4fd920f1b27a865c80aa8106
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-05 21:59:58 -0400

    the variable is creating new lines as it's read into GH Actions. hardcoding to isolate err

--- environment/build.sh
+++ environment/build.sh
@@ -3,11 +3,11 @@ echo Building url-shortener
 
 IMAGE="public.ecr.aws/j2m0y8o3/url-shortener"
 
-docker pull ${IMAGE}:dependencies || true
+docker pull public.ecr.aws/j2m0y8o3/url-shortener:dependencies || true
 
-DOCKER_BUILDKIT=1 docker build --target dependencies --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ${IMAGE}:dependencies -t ${IMAGE}:dependencies -f environment/Dockerfile .
+DOCKER_BUILDKIT=1 docker build --target dependencies --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies -t public.ecr.aws/j2m0y8o3/url-shortener:dependencies -f environment/Dockerfile .
 
-docker push ${IMAGE}:dependencies
+docker push public.ecr.aws/j2m0y8o3/url-shortener:dependencies
 
 # Build and tag with appropriate values
-DOCKER_BUILDKIT=1 docker build --target full --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from ${IMAGE}:dependencies -t url-shortener:latest -t url-shortener:$(git rev-parse --short HEAD) -f environment/Dockerfile .
+DOCKER_BUILDKIT=1 docker build --target full --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies -t url-shortener:latest -t url-shortener:$(git rev-parse --short HEAD) -f environment/Dockerfile .
