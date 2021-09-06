09a465fb88e60d5ae034b308b2633b9cd771dce5
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-06 16:17:39 +0000

    added space to validate user input

--- src/url_shortener/url_shortener.py
+++ src/url_shortener/url_shortener.py
@@ -5,11 +5,23 @@ from random import choice
 class UrlShortener:
     @classmethod
     def submit_url_and_get_shortcode(
-        cls, url: str, desired_shortcode: str = None
+        cls, url: str, user_shortcode: str = None
     ) -> str:
-        if desired_shortcode is not None:
-            return desired_shortcode
+        if user_shortcode is not None:
+            cls._try_to_process_user_shortcode(user_shortcode)
+            return user_shortcode
+
         return cls._random_string_of_length_n(6)
+
+    @classmethod
+    def _try_to_process_user_shortcode(cls, user_shortcode: str) -> str:
+        if cls._shortcode_is_valid(user_shortcode):
+            return user_shortcode
+        raise NotImplementedError
+
+    @staticmethod
+    def _shortcode_is_valid() -> bool:
+        ...
 
     @staticmethod
     def _random_string_of_length_n(n: int) -> str:
