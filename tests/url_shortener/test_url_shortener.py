1620fbb128e33b970af557db975f157fc07f5769
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-06 16:07:07 +0000

    added test to check len

--- tests/url_shortener/test_url_shortener.py
+++ tests/url_shortener/test_url_shortener.py
@@ -11,3 +11,13 @@ class TestUrlShortener:
 
         # Assert
         assert type(result) == str
+
+    def test_can_submit_url_and_get_string_of_length_6(self):
+        # Arrange
+        url_shortener = UrlShortener()
+
+        # Act
+        result = url_shortener.submit_url_and_get_shortcode("some_url")
+
+        # Assert
+        assert len(result) == 6
