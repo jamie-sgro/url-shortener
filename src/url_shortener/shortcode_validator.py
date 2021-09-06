019fb053df6a6b51e7f19bcc11995b57945fc870
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-06 16:31:02 +0000

    added some basic shortcode validation

--- src/url_shortener/shortcode_validator.py
+++ src/url_shortener/shortcode_validator.py
@@ -10,11 +10,14 @@ class ValidatorResult:
 class ShortcodeValidator:
     @staticmethod
     def is_valid(shortcode: str) -> ValidatorResult:
+        if type(shortcode) is not str:
+            return ValidatorResult(
+                False, "User submitted shortcodes must be of type: string."
+            )
+
         if len(shortcode) <= 4:
             return ValidatorResult(
                 False, "User submitted shortcodes must be at least 4 characters long."
             )
-
-        # Add new validation here
 
         return ValidatorResult(False, "User submitted shortcodes is valid.")
--- tests/url_shortener/test_shortcode_validator.py
+++ tests/url_shortener/test_shortcode_validator.py
@@ -2,5 +2,14 @@ from src.url_shortener.shortcode_validator import ShortcodeValidator
 
 
 class TestShortcodeValidator:
+    def test_not_of_type_string_invalid(self):
+        assert ShortcodeValidator.is_valid(123).status == False  # type: ignore
+        assert ShortcodeValidator.is_valid(123456789).status == False  # type: ignore
+        assert ShortcodeValidator.is_valid(1.23).status == False  # type: ignore
+        assert ShortcodeValidator.is_valid(["abcd"]).status == False  # type: ignore
+        assert ShortcodeValidator.is_valid(["abcd", "efgh"]).status == False  # type: ignore
+        assert ShortcodeValidator.is_valid(("abcd", "efgh")).status == False  # type: ignore
+
     def test_short_strings_invalid(self):
         assert ShortcodeValidator.is_valid("123").status == False
+        assert ShortcodeValidator.is_valid("abc").status == False
