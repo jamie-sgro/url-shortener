1de28d417e26f6cb446cfdd3b6415d6d56134b75
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-05 22:07:49 -0400

    attempting to add backslashed to see if GH actions handles the script the same

--- environment/build.sh
+++ environment/build.sh
@@ -5,7 +5,12 @@ IMAGE="public.ecr.aws/j2m0y8o3/url-shortener"
 
 docker pull public.ecr.aws/j2m0y8o3/url-shortener:dependencies || true
 
-DOCKER_BUILDKIT=1 docker build --target dependencies --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies -t public.ecr.aws/j2m0y8o3/url-shortener:dependencies -f environment/Dockerfile .
+DOCKER_BUILDKIT=1 docker build \
+--target dependencies \
+--build-arg BUILDKIT_INLINE_CACHE=1 \
+--cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
+-t public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
+-f environment/Dockerfile .
 
 docker push public.ecr.aws/j2m0y8o3/url-shortener:dependencies
 
