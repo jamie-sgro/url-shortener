883253e17a517eaff1d43d39d645a7428ab5a013
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-05 20:59:13 -0400

    added optional ability to pass branch name to bash script

--- environment/build.sh
+++ environment/build.sh
@@ -2,18 +2,24 @@
 echo Building url-shortener
 
 # Get branch name, and convert `#` and `/` into `_`
-branch_name=$(git symbolic-ref -q HEAD)
-branch_name=${branch_name##refs/heads/}
-branch_name=${branch_name:-HEAD}
-branch_name=$(echo ${branch_name} | tr '#/' _)
-echo Attempting to pull image for branch ${branch_name}
+parse_branch_name () {
+  temp_branch_name=$(git symbolic-ref -q HEAD)
+  temp_branch_name=${temp_branch_name##refs/heads/}
+  temp_branch_name=${temp_branch_name:-HEAD}
+  temp_branch_name=$(echo ${temp_branch_name} | tr '#/' _)
+  echo $temp_branch_name
+}
 
-# Pull the latest runtime image from remote repository
-docker pull public.ecr.aws/j2m0y8o3/url-shortener:${branch_name} || true
+BRANCH_NAME=${1:-$(parse_branch_name)}
 
-# Build and tag with appropriate values
+echo Attempting to pull image for branch ${BRANCH_NAME}
+
+# # Pull the latest runtime image from remote repository
+docker pull public.ecr.aws/j2m0y8o3/url-shortener:${BRANCH_NAME} || true
+
+# # Build and tag with appropriate values
 docker build \
-  --cache-from public.ecr.aws/j2m0y8o3/url-shortener:${branch_name} \
+  --cache-from public.ecr.aws/j2m0y8o3/url-shortener:${BRANCH_NAME} \
   -t url-shortener:latest \
   -t url-shortener:$(git rev-parse --short HEAD) \
   -f environment/Dockerfile .
