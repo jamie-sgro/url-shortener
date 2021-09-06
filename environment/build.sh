2a8f0dfd009c826e82448f811650a201b54e985c
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-05 20:45:28 -0400

    updated build script to first pull and attempt cache

--- environment/build.sh
+++ environment/build.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 echo Building url-shortener
 
+# Get branch name, and convert `#` and `/` into `_`
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+branch_name=$(echo ${branch_name} | tr '#/' _)
+echo Attempting to pull image for branch ${branch_name}
+
+# Pull the latest runtime image from remote repository
+docker pull public.ecr.aws/j2m0y8o3/url-shortener:${branch_name} || true
+
 # Build and tag with appropriate values
-docker build -t url-shortener:latest -t url-shortener:$(git rev-parse --short HEAD) -f environment/Dockerfile .
+docker build \
+  --cache-from public.ecr.aws/j2m0y8o3/url-shortener:${branch_name} \
+  -t url-shortener:latest \
+  -t url-shortener:$(git rev-parse --short HEAD) \
+  -f environment/Dockerfile .
