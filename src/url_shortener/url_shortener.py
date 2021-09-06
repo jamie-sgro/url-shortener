a0dae6ec4c99f94e5fa3a8699b543f32af50075d
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-06 16:07:19 +0000

    added method to retrieve random default string

--- src/url_shortener/url_shortener.py
+++ src/url_shortener/url_shortener.py
@@ -1,4 +1,12 @@
+import string
+from random import choice
+
+
 class UrlShortener:
     @classmethod
     def submit_url_and_get_shortcode(cls, url: str) -> str:
-        return "string"
+        return cls._random_string_of_length_n(6)
+
+    @staticmethod
+    def _random_string_of_length_n(n: int) -> str:
+        return "".join(choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for i in range(n))
