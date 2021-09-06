c4be2bfe8a0eb43edf1275bbb15e9f109999004d
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-05 21:22:14 -0400

    changed build script to make use of multistage caching

--- environment/build.sh
+++ environment/build.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 echo Building url-shortener
 
-# Get branch name, and convert `#` and `/` into `_`
-parse_branch_name () {
-  temp_branch_name=$(git symbolic-ref -q HEAD)
-  temp_branch_name=${temp_branch_name##refs/heads/}
-  temp_branch_name=${temp_branch_name:-HEAD}
-  temp_branch_name=$(echo ${temp_branch_name} | tr '#/' _)
-  echo $temp_branch_name
-}
+docker pull public.ecr.aws/j2m0y8o3/url-shortener:dependencies || true
 
-TAG_NAME=${1:-$(parse_branch_name)}
+docker build --target dependencies --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies -t url-shortener:dependencies -t public.ecr.aws/j2m0y8o3/url-shortener:dependencies
 
-echo Attempting to pull image for branch ${TAG_NAME}
+docker push public.ecr.aws/j2m0y8o3/url-shortener:dependencies
 
-# # Pull the latest runtime image from remote repository
-docker pull public.ecr.aws/j2m0y8o3/url-shortener:${TAG_NAME} || true
-
-# # Build and tag with appropriate values
-docker build --cache-from public.ecr.aws/j2m0y8o3/url-shortener:${TAG_NAME} -t url-shortener:latest -t url-shortener:$(git rev-parse --short HEAD) -f environment/Dockerfile .
+# Build and tag with appropriate values
+DOCKER_BUILDKIT=1 docker build --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies -t url-shortener:latest -t url-shortener:$(git rev-parse --short HEAD) -f environment/Dockerfile .
