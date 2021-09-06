4c9fe901244407e29ca1ca324fe269da0e03cd47
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-06 16:00:15 +0000

    added test case to submit urlk

--- tests/url_shortener/test_url_shortener.py
+++ tests/url_shortener/test_url_shortener.py
@@ -1,2 +1,13 @@
+from src.url_shortener.url_shortener import UrlShortener
+
+
 class TestUrlShortener:
-    ...
+    def test_can_submit_url_and_get_string():
+        # Arrange
+        url_shortener = UrlShortener()
+
+        # Act
+        result = url_shortener.submit_url_and_get_shortcode("some_url")
+
+        # Assert
+        assert type(result) == str
