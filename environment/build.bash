12354a9929daaad3bd4751cc46fecd8ab744f3c8
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-05 22:25:40 -0400

    attempting to use bash variables

--- environment/build.bash
+++ environment/build.bash
@@ -3,22 +3,22 @@ echo Building url-shortener
 
 IMAGE="public.ecr.aws/j2m0y8o3/url-shortener"
 
-docker pull public.ecr.aws/j2m0y8o3/url-shortener:dependencies || true
+docker pull ${IMAGE}:dependencies || true
 
 DOCKER_BUILDKIT=1 docker build \
   --target dependencies \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
-  --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
-  -t public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
+  --cache-from ${IMAGE}:dependencies \
+  -t ${IMAGE}:dependencies \
   -f environment/Dockerfile .
 
-docker push public.ecr.aws/j2m0y8o3/url-shortener:dependencies
+docker push ${IMAGE}:dependencies
 
 # Build and tag with appropriate values
 DOCKER_BUILDKIT=1 docker build \
   --target full \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
-  --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
+  --cache-from ${IMAGE}:dependencies \
   -t url-shortener:latest \
   -t url-shortener:$(git rev-parse --short HEAD) \
   -f environment/Dockerfile .
