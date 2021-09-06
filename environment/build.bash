5f10cd924d28246eeeadf6b73c4ca891b0be166e
jamie-sgro <jamie.sgro@uwaterloo.ca>
2021-09-05 22:23:28 -0400

    added new multilines after reaching stable builds

--- environment/build.bash
+++ environment/build.bash
@@ -6,13 +6,19 @@ IMAGE="public.ecr.aws/j2m0y8o3/url-shortener"
 docker pull public.ecr.aws/j2m0y8o3/url-shortener:dependencies || true
 
 DOCKER_BUILDKIT=1 docker build \
---target dependencies \
---build-arg BUILDKIT_INLINE_CACHE=1 \
---cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
--t public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
--f environment/Dockerfile .
+  --target dependencies \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
+  --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
+  -t public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
+  -f environment/Dockerfile .
 
 docker push public.ecr.aws/j2m0y8o3/url-shortener:dependencies
 
 # Build and tag with appropriate values
-DOCKER_BUILDKIT=1 docker build --target full --build-arg BUILDKIT_INLINE_CACHE=1 --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies -t url-shortener:latest -t url-shortener:$(git rev-parse --short HEAD) -f environment/Dockerfile .
+DOCKER_BUILDKIT=1 docker build \
+  --target full \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
+  --cache-from public.ecr.aws/j2m0y8o3/url-shortener:dependencies \
+  -t url-shortener:latest \
+  -t url-shortener:$(git rev-parse --short HEAD) \
+  -f environment/Dockerfile .
